ef056accaf44c6e81ae5c7b68d2a11251cc4e529
John Cokos <highlander.44@gmail.com>
2019-07-03 17:14:50 -0700

    Floaty Store react app uploaded

---
+++ class-28/demo/floaty-store/src/app.js
@@ -0,0 +1,94 @@
+import React, {useState, useEffect} from 'react';
+import logo from './logo.svg';
+import './app.scss';
+
+import mockData from './mock.json';
+
+const API = 'http://172.16.0.219:5000/api/v1/customers'
+
+function People() {
+
+  const [people, setPeople] = useState([]);
+
+  const _getPeople = () => {
+    // setPeople(mockData)
+    // fetch from brook
+    fetch( API, {
+      mode:'cors',
+    })
+    .then( data => data.json() )
+    .then( ppl => setPeople(ppl) )
+    .catch( console.error );
+
+  };
+
+  const _toggleStatus = (e) => {
+    e.preventDefault();
+    let id = e.target.id;
+    // setPeople( people.map( (person) =>
+    //   person.id !== id ? person : {...person, vip:!person.vip}
+    // ));
+
+    // patch to Brooks api
+    fetch( `${API}/${id}/status`, {
+      mode:'cors',
+      method: 'PATCH'
+    })
+    .then(data => data.json())
+    .then(person => {
+      setPeople( people.map( (entry) => {
+          return entry.id === id ? person : entry;
+        }
+      ));
+    })
+    .catch( console.error );
+
+  };
+
+  useEffect(_getPeople, []);
+
+  return (
+    <ul>
+      {people.map( (person) =>
+        <li className={`vip-${person.vip}`} key={person.id}>
+          <details>
+            <summary>
+              <span>{person.name}</span>
+              <span id={person.id} onClick={_toggleStatus}>{person.vip.toString()}</span>
+
+            </summary>
+            <History history={person.history} />
+          </details>
+        </li>
+      )}
+    </ul>
+  )
+}
+
+function History(props) {
+  let history = props.history || [];
+  return (
+    <section>
+      {history.map( (item,idx) =>
+        <div>
+          <span>{item.itemName}</span>
+          <span>{item.price}</span>
+        </div>
+      )}
+    </section>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <header>Floaty Store</header>
+      <main>
+        <People />
+      </main>
+      <footer>&copy; 2019 Brookr</footer>
+      </>
+  );
+}
+
+export default App;
