79a121068bd61556b5486655931f9ac0f3784cd5
mnfmnfm <mnfmnfm@gmail.com>
2019-07-12 10:03:15 -0700

    updated version of lambda function

--- class-31/demo/src/main/java/demo/Library.java
+++ class-31/demo/src/main/java/demo/Library.java
@@ -6,7 +6,30 @@ package demo;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
+import java.util.HashSet;
+
 public class Library {
+
+    // { "words": string, "shouldRunWholeMethod": boolean }
+
+    public String handleRepeatedWordRequest(RepeatedWordRequest request) {
+        if(request.shouldRunWholeMethod) {
+            return getRepeatedWord(request.words);
+        } else {
+            return "done";
+        }
+    }
+    public String getRepeatedWord(String words) {
+        HashSet<String> wordSet = new HashSet<>();
+        for (String word : words.split(" ")) {
+            if (wordSet.contains(word)) {
+                return word;
+            } else {
+                wordSet.add(word);
+            }
+        }
+        return null;
+    }
     public int addNumbers(PairOfNumbers numbers, Context c) {
         LambdaLogger logger = c.getLogger();
         logger.log("received the numbers " + numbers.a + " and " + numbers.b);
@@ -14,6 +37,18 @@ public class Library {
     }
 }
 
+class RepeatedWordRequest {
+    String words;
+    boolean shouldRunWholeMethod;
+
+    public void setWords(String words) {
+        this.words = words;
+    }
+
+    public void setShouldRunWholeMethod(boolean b) {
+        this.shouldRunWholeMethod = b;
+    }
+}
 class PairOfNumbers{
     int a;
     int b;
